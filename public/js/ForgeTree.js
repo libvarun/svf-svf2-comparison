c80061914fc5f7483845f9d3c236c10aab75ef04
varun patil <varun.patil578@gmail.com>
2020-11-13 10:25:51 +0530

    initial commit

---
+++ public/js/ForgeTree.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+  launchViewer('dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Zm9yZ2Vzc3IvMjEwJTIwS2luZyUyME9mZmljZS5ydnQ');
+});
---
+++ public/js/svf2/ForgeTree.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+  launchViewer('dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3ZmMnRlc3R2YXJ1bi8yMTAlMjBLaW5nJTIwT2ZmaWNlLnJ2dA');
+});
